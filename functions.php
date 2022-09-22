9da142bf5661f5298b636266e877b89d5bfe9e9d
Tatenda Munenge <tatenda.munenge@stickee.co.uk>
2022-09-22 21:05:24 +0200

    theme asserts and base styles

--- functions.php
+++ functions.php
@@ -138,10 +138,11 @@ add_action( 'widgets_init', 'bloggar_widgets_init' );
  * Enqueue scripts and styles.
  */
 function bloggar_scripts() {
+	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css' , array(), null );
 	wp_enqueue_style( 'bloggar-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'bloggar-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'bloggar-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_style( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js' , array(), null );
+	wp_enqueue_script( 'bloggar-navigation', get_template_directory_uri() . '/assets/js/main.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
