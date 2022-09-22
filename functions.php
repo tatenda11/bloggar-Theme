c16a7124b99c1d63d7968c11cd7ed6e967e62431
Tatenda Munenge <tatenda.munenge@stickee.co.uk>
2022-09-22 20:12:05 +0200

    assets directory structure

--- functions.php
+++ functions.php
@@ -141,7 +141,7 @@ function bloggar_scripts() {
 	wp_enqueue_style( 'bloggar-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'bloggar-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'bloggar-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'bloggar-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
